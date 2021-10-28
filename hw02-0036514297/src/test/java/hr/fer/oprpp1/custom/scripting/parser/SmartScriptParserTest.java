59078413e93d4d56d532503e7b0da39004f3b53f
karlokovac <kovac.karlo5@gmail.com>
2021-10-28 17:20:19 +0200

    Parser test

--- hw02-0036514297/src/test/java/hr/fer/oprpp1/custom/scripting/parser/SmartScriptParserTest.java
+++ hw02-0036514297/src/test/java/hr/fer/oprpp1/custom/scripting/parser/SmartScriptParserTest.java
@@ -1,6 +1,5 @@
 package hr.fer.oprpp1.custom.scripting.parser;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -12,9 +11,4 @@ public class SmartScriptParserTest {
 		assertThrows(NullPointerException.class, () -> new SmartScriptParser(null));
 	}
 
-	@Test
-	public void testEmptyDocument() {
-		var parser = new SmartScriptParser("Rimac");
-		assertEquals(1, parser.getDocumentNode().numberOfChildren());
-	}
 }
