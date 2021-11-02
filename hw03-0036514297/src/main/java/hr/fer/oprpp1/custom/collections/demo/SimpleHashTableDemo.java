94e96a24491c38b69739577d0e508f677e5f5ac1
karlokovac <kovac.karlo5@gmail.com>
2021-11-02 21:26:38 +0100

    Demo combined

--- hw03-0036514297/src/main/java/hr/fer/oprpp1/custom/collections/demo/SimpleHashTableDemo.java
+++ hw03-0036514297/src/main/java/hr/fer/oprpp1/custom/collections/demo/SimpleHashTableDemo.java
@@ -1,5 +1,7 @@
 package hr.fer.oprpp1.custom.collections.demo;
 
+import java.util.Iterator;
+
 import hr.fer.oprpp1.custom.collections.SimpleHashtable;
 
 public class SimpleHashTableDemo {
@@ -17,5 +19,24 @@ public class SimpleHashTableDemo {
 		System.out.println("Kristina's exam grade is: " + kristinaGrade); // writes: 5
 		// What is collection's size? Must be four!
 		System.out.println("Number of stored pairs: " + examMarks.size()); // writes: 4
+
+		for (SimpleHashtable.TableEntry<String, Integer> pair : examMarks) {
+			System.out.printf("%s => %d%n", pair.getKey(), pair.getValue());
+		}
+
+		for (SimpleHashtable.TableEntry<String, Integer> pair1 : examMarks) {
+			for (SimpleHashtable.TableEntry<String, Integer> pair2 : examMarks) {
+				System.out.printf("(%s => %d) - (%s => %d)%n", pair1.getKey(), pair1.getValue(), pair2.getKey(),
+						pair2.getValue());
+			}
+		}
+
+		Iterator<SimpleHashtable.TableEntry<String, Integer>> iter = examMarks.iterator();
+		while (iter.hasNext()) {
+			SimpleHashtable.TableEntry<String, Integer> pair = iter.next();
+			System.out.printf("%s => %d%n", pair.getKey(), pair.getValue());
+			iter.remove();
+		}
+		System.out.printf("Veličina: %d%n", examMarks.size());
 	}
 }
